8c771c341293f102b28db0de022925d018580ba1
Arsalan Peerzada <peerzada_1994@live.com>
2019-05-02 21:20:20 +0500

    new commit

---
+++ practice/switch part 2/switch part 2/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace switch_part_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            
+            
+           /* local variable definition */
+            Console.WriteLine("input grade");
+            char grade = Convert.ToChar(Console.ReadLine());
+
+            switch (grade)
+            {
+                case 'A':
+                    Console.WriteLine("Excellent!");
+                    break;
+                case 'a':
+                    Console.WriteLine("Excellent!");
+                    break;
+                case 'C':
+                    Console.WriteLine("Well done");
+                    break;
+                case 'c':
+                    Console.WriteLine("Well done");
+                    break;
+                case 'D':
+                    Console.WriteLine("You passed");
+                    break;
+                case 'd':
+                    Console.WriteLine("You passed");
+                    break;
+                case 'F':
+                    Console.WriteLine("Better try again");
+                    break;
+                case 'f':
+                    Console.WriteLine("Better try again");
+                    break;
+                default:
+                    Console.WriteLine("Invalid grade");
+                    break;
+            }
+            Console.WriteLine("Your grade is =  {0}", grade);
+            Console.ReadLine();
+            //////////////////////////////
+        }
+    }
+}
---
+++ practice/switch part 4/switch part 4/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace switch_part_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+
+
+            int b = int.Parse(Console.ReadLine());
+
+            if (a > b)
+            {
+                Console.Write("tru\n");
+            }
+            else if (a < b)
+            {
+                Console.Write("tru\n");
+            }
+
+            else if (a >= b)
+            {
+                Console.Write("tru\n");
+            }
+            else if (a <= b)
+            {
+                Console.Write("tru\n");
+            }
+            else if (a != b)
+
+                Console.Write("tru\n");
+
+            else if (a == b)
+            {
+                Console.Write("tru \n");
+            }
+        }
+    }
+}
